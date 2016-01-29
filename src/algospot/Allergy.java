ccafa5b2eb9b3658eeee3f1be591f3324925e1eb
진유리 <yuriya.jin@samsung.com>
2016-01-29 17:09:16 +0900

    allergy 풀이중

--- src/algospot/Allergy.java
+++ src/algospot/Allergy.java
@@ -17,16 +17,27 @@ public class Allergy {
 			int m = sc.nextInt();
 			
 			Map<String, Integer> names = new HashMap<>();
-			int[][] map = new int[n][m];
+			int[][] map = new int[m][n];
+			// 이름, index 받기
 			for(int i=0; i<n; ++i){
 				names.put(sc.next(), i);
 			}
 			for(int i=0; i<m; ++i){
 				int idx = sc.nextInt();
-				map[i][idx] = names.get(sc.next());
+				for(int j=0; j<idx; ++j){
+					map[i][names.get(sc.next())] = 1;
+				}
 			}
+			int foodCnt = 0;
+			do{
+				int[] eatMap = new int[n];
+				for(int i=0; i<m; ++i){
+					for(int j=0; j<n; ++j){
+						
+					}
+				}
+			}while(foodCnt != n);
 		}
-		
 		sc.close();
 	}
 }
